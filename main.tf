fd3bcf801d18332455141da79df31704385e5e5d
Andrew Benz <andrew.benz@auctane.com>
2024-12-13 14:46:06 -0600

    Autoscaling

--- main.tf
+++ main.tf
@@ -33,20 +33,23 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
+
+  image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
 
-module "blog-alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
@@ -76,25 +79,6 @@ module "blog-alb" {
         target_group_key = "ex-instance"
       }
     }
-
-    # ex-http-https-redirect = {
-    #   port     = 80
-    #   protocol = "HTTP"
-    #   redirect = {
-    #     port        = "443"
-    #     protocol    = "HTTPS"
-    #     status_code = "HTTP_301"
-    #   }
-    # }
-    # ex-https = {
-    #   port            = 443
-    #   protocol        = "HTTPS"
-    #   certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
-    #   forward = {
-    #     target_group_key = "ex-instance"
-    #   }
-    # }
   }
 
   tags = {
